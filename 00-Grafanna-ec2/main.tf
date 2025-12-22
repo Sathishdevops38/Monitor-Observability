a4c3c3b2852eab93d3b2dc68b3b034d3a27dd0d5
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-22 10:04:14 +0530

    grafana ec2

---
+++ 00-Grafanna-ec2/main.tf
@@ -0,0 +1,40 @@
+module "grafana" {  
+  source = "git::https://github.com/Sathishdevops38/terraform-modules.git//20-bastion-module?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  subnet_id = local.subnet_id  
+  instance_type = var.instance_type
+  sg_ids =   [local.sg_id]
+  iam_profile_name = aws_iam_instance_profile.grafana.name
+  root_volume_size = 50
+  root_volume_type = "gp3"
+
+  user_data = file("grafana.sh")
+  tags= merge(
+    var.grafana_tags,
+    local.common_tags,{
+      Name =  "${local.common_name_suffix}-grafana"
+    }
+  )  
+}
+
+resource "aws_iam_instance_profile" "grafana" {
+  name = "grafana"
+  role = "BastionTerraformAdmin"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "grafana" {
+  security_group_id = local.sg_id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "all_access" {
+  security_group_id = local.sg_id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "Allow all inbound traffic from IPv4"
+}
